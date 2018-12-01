b407ef6c3b16a790de5621d6d2bbb23648256c52
Emil Olsson <emilolsson93@gmail.com>
2018-12-01 17:33:12 +0100

    Init repo | restrucutre projects

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+[assembly: AssemblyDescription("Eols.EPiGraphQL.Core an EPiServer tool")]
+[assembly: AssemblyTitle("Eols.EPiGraphQL.Core")]
+[assembly: AssemblyProduct("Eols.EPiGraphQL.Core")]
+[assembly: AssemblyCopyright("Copyright 2018 Emil Olsson")]
+[assembly: AssemblyTrademark("3f137a73c911af0aa25fc26dc128c581f3f280fa")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyInformationalVersion("0.5.0.0")]
+[assembly: CLSCompliant(false)]
